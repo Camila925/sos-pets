88042ef65ad0153df1c8f2eca70d003c626f404a
Camila925 <camilamarques_silva132@hotmail.com>
2025-11-27 21:50:02 -0300

    correção de bugs pagina triplicada

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 const form = document.getElementById("registroForm");
 const lista = document.getElementById("listaRegistros");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit"), (e) => {
     e.preventDefault();
 
     const nome = document.getElementById("nome").value;
@@ -15,32 +14,9 @@ form.addEventListener("submit", (e) => {
         <h3>${nome || "Animal sem nome"}</h3>
         <p><strong>Local:</strong> ${local}</p>
         <p>${desc}</p>
-    `;
+`;
 
     lista.appendChild(card);
 
     form.reset();
-=======
-const form = document.getElementById("registroForm");
-const lista = document.getElementById("listaRegistros");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const nome = document.getElementById("nome").value;
-    const local = document.getElementById("local").value;
-    const desc = document.getElementById("descricao").value;
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
-        <h3>${nome || "Animal sem nome"}</h3>
-        <p><strong>Local:</strong> ${local}</p>
-        <p>${desc}</p>
-    `;
-
-    lista.appendChild(card);
-
-    form.reset();
->>>>>>> 545e30b3a1c96d5b40a25d5aa9bab8aa89793a04
-});
+}
